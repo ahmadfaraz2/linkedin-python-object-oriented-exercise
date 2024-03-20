b0d605a460d44011e68bcb004f78b18ca69c8bac
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-20 21:22:59 +0500

    Learnn about abstraction and where should we use it

--- Start/Ch 2/abstract_start.py
+++ Start/Ch 2/abstract_start.py
@@ -1,11 +1,14 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
 
+from abc import ABC, abstractmethod
 
-class GraphicShape:
+
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod  # This will enforce other subclasses to use it
     def calcArea(self):
         pass
 
@@ -14,15 +17,29 @@ class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calcArea(self):
+        return 3.14 * (self.radius**2)
+
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return self.side * self.side
 
-g = GraphicShape()
+
+# g = GraphicShape() # So the class that are child class of "ABC" can't be instantiate
 
 c = Circle(10)
 print(c.calcArea())
 s = Square(12)
 print(s.calcArea())
+
+
+"""
+    So we can use Abstraction where we want to make a Base class
+    but we don't want to make instances of this class but we can
+    enforce the constraints that there are certain methods in
+    the Base class that have to be implemented in subclasses.
+"""
