ff42b2084acd8f25f8395ec24a4a7a08b6768e5c
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-21 21:51:26 +0500

    Alhumdulillah completed this chanllenge and implemented the concepts I learned in previous chapters

--- Start/Ch 3/challenge.py
+++ Start/Ch 3/challenge.py
@@ -23,6 +23,12 @@ class Stock(Asset):
         self.company = company
         self.ticker = ticker
 
+    def __str__(self):
+        return f"{self.ticker}: {self.company} -- ${self.price}"
+
+    def __lt__(self, value):
+        return self.price < value.price
+
 
 class Bond(Asset):
     def __init__(self, price, description, duration, yieldamt):
@@ -31,20 +37,28 @@ class Bond(Asset):
         self.duration = duration
         self.yieldamt = yieldamt
 
+    def __str__(self):
+        return (
+            f"{self.description}: {self.duration}yr : {self.price} : {self.yieldamt}%"
+        )
+
+    def __lt__(self, value):
+        return self.price < value.price
+
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 stocks = [
     Stock("MSFT", 342.0, "Microsoft Corp"),
     Stock("GOOG", 135.0, "Google Inc"),
     Stock("META", 275.0, "Meta Platforms Inc"),
-    Stock("AMZN", 120.0, "Amazon Inc")
+    Stock("AMZN", 120.0, "Amazon Inc"),
 ]
 
 bonds = [
     Bond(95.31, "30 Year US Treasury", 30, 4.38),
     Bond(96.70, "10 Year US Treasury", 10, 4.28),
     Bond(98.65, "5 Year US Treasury", 5, 4.43),
-    Bond(99.57, "2 Year US Treasury", 2, 4.98)
+    Bond(99.57, "2 Year US Treasury", 2, 4.98),
 ]
 
 stocks.sort()
