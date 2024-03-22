869a1f5d512c08bb1feb0acd54752cb9aaf6f848
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-22 22:12:42 +0500

    Learn aobut how to make Data classes immutable by setting forzen parameter = True in dataclass decorator

--- Start/Ch 4/immutable_start.py
+++ Start/Ch 4/immutable_start.py
@@ -4,16 +4,21 @@
 from dataclasses import dataclass
 
 
-@dataclass()  # TODO: "The "frozen" parameter makes the class immutable
+@dataclass(frozen=True)  # TODO: "The "frozen" parameter makes the class immutable
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
 
+    def some_func(self, new_val):
+        self.value1 = new_val
 
-obj = ImmutableClass()
-print(obj.value1)
+
+obj = ImmutableClass("It's a new value", 50)
+print(obj.value1, obj.value2)
 
 # TODO: attempting to change the value of an immutable class throws an exception
-
+# obj.value2 = 10
+# print(obj)
 
 # TODO: even functions within the class can't change anything
+# obj.some_func("New value")
