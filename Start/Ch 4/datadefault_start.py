3c254ffa7c41befbde6abde85886141144a28069
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-22 21:58:30 +0500

    Learn about how to define default values when attributes are declared manually and using field() func

--- Start/Ch 4/datadefault_start.py
+++ Start/Ch 4/datadefault_start.py
@@ -1,13 +1,31 @@
 # Python Object Oriented Programming by Joe Marini course example
 # implementing default values in data classes
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import random
+
+
+def func_price():
+    randint = random.randrange(20, 40)
+    return randint
 
 
 @dataclass
 class Book:
     # you can define default values when attributes are declared
-    title: str
-    author: str
-    pages: int
-    price: float
+    title: str = "No Title"
+    author: str = "No Author"
+    pages: int = 0
+    # price: float = field(default=10.0)
+    price: float = field(default_factory=func_price)
+
+
+b1 = Book()
+print(b1)
+
+b2 = Book("The One Thing", "Gary Keller and J.Papason", 240)
+b3 = Book("Sometimes You Win Sometimes You Lose", "John C.Maxwell", 250)
+
+
+print(b2)
+print(b3)
