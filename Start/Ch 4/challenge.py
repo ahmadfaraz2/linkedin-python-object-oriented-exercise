4352f6abca1d5364cb93d9b8b377765a497f3df9
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-22 22:43:08 +0500

    Alhumdulillah completed this challenge and implement the learnings I have learned from the pervious chapters

--- Start/Ch 4/challenge.py
+++ Start/Ch 4/challenge.py
@@ -5,42 +5,58 @@
 # The subclasses are required to override the magic method
 # that makes them sortable
 
-class Asset():
-    pass
-    
+from dataclasses import dataclass
 
+
+@dataclass
+class Asset:
+    price: float
+
+
+@dataclass
 class Stock(Asset):
-    pass
+    ticker: str
+    company: str
+
+    def __lt__(self, value):
+        return self.price < value.price
 
 
+@dataclass
 class Bond(Asset):
-    pass
+    description: str
+    duration: int
+    yieldamt: float
+
+    def __lt__(self, other):
+        return self.yieldamt < other.yieldamt
+
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 stocks = [
     Stock("MSFT", 342.0, "Microsoft Corp"),
     Stock("GOOG", 135.0, "Google Inc"),
     Stock("META", 275.0, "Meta Platforms Inc"),
-    Stock("AMZN", 120.0, "Amazon Inc")
+    Stock("AMZN", 120.0, "Amazon Inc"),
 ]
 
 bonds = [
     Bond(95.31, "30 Year US Treasury", 30, 4.38),
     Bond(96.70, "10 Year US Treasury", 10, 4.28),
     Bond(98.65, "5 Year US Treasury", 5, 4.43),
-    Bond(99.57, "2 Year US Treasury", 2, 4.98)
+    Bond(99.57, "2 Year US Treasury", 2, 4.98),
 ]
 
 try:
-   ast = Asset(100.0)
+    ast = Asset(100.0)
 except:
-   print("Can't instantiate Asset!")
+    print("Can't instantiate Asset!")
 
 stocks.sort()
 bonds.sort()
 
 for stock in stocks:
-   print(stock)
+    print(stock)
 print("-----------")
 for bond in bonds:
-   print(bond)
+    print(bond)
